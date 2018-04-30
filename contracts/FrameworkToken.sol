036f36ca6b9f9f869dc1c4f0fc1a580b1e6f02c2
Jason Konicki <jkonicki@gopivotal.com>
2018-04-30 01:53:32 -0400

    Removing ERC223 from token

--- contracts/FrameworkToken.sol
+++ contracts/FrameworkToken.sol
@@ -4,9 +4,8 @@ import 'zeppelin-solidity/contracts/ownership/Contactable.sol';
 
 import 'zeppelin-solidity/contracts/token/ERC20/CappedToken.sol';
 import 'zeppelin-solidity/contracts/token/ERC20/PausableToken.sol';
-import './ERC223/ERC223Token.sol';
 
-contract FrameworkToken is CappedToken, ERC223Token, PausableToken, Contactable {
+contract FrameworkToken is CappedToken, PausableToken, Contactable {
   string public name = "Framework";
   string public symbol = "FRWK";
   uint8 public decimals = 18;
